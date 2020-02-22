d25733b22332d55f59a490d0e3d9bc6a1a0caa18
91let <91let@naver.com>
2020-02-22 12:29:04 +0900

    rebuild commit

---
+++ DefaultBack/src/main/java/com/mySetting/DefaultBackApplication.java
@@ -0,0 +1,13 @@
+package com.mySetting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefaultBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefaultBackApplication.class, args);
+	}
+
+}
---
+++ DefaultBack/src/test/java/com/mySetting/DefaultBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mySetting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefaultBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
